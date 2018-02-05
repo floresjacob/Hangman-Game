100032fa06b7aa8fc5d489dbde2d8d2a0939dc52
Jacob Ethan Flores <jacob.e.flores562@gmail.com>
2018-02-05 15:23:49 -0800

    added all functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,79 @@
 window.onload = function () {
 
-var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-'t', 'u', 'v', 'w', 'x', 'y', 'z']
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h",
+"i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+"t", "u", "v", "w", "x", "y", "z"]
+var word = ["p","a","d","d","i","e","s"]
 
-var a_canvas = document.getElementById("a")
-var context = a_canvas.getContext("2d")
-var word = ""
+var bar = document.getElementById("choice")
+bar.setAttribute("style", "margin: auto;")
+var letArray = []
+var attempts = 5
+var choiceBar = function() {
+    
+    for(i=0; i<alphabet.length; i++){
+        letter = document.createElement("button")
+        letter.setAttribute("type","button")
+        letter.classList.add("btn")
+        letter.classList.add("btn-outline-success")
+        letter.setAttribute("id", "press")
+        letter.innerHTML = alphabet[i]
+        select();
+        bar.appendChild(letter)  
+    }
+}
+
+var select = function() {
+    letter.onclick = function() {
+        this.setAttribute("style", "background-color: black")
+        letArray.push(this.innerHTML)
+        console.log(letArray)
+        for(i=0; i<word.length; i++){
+            var bool = false
+            if(this.innerHTML === word[i]){
+                var hit = document.getElementById(word[i]+"_")
+                hit.innerHTML = this.innerHTML
+                bool = true
+                this.onclick = null
+            }
+            else if(i===word.length-1 && bool===false){
+                sketchMan[attempts]()
+                attempts--
+                if(attempts < 0){
+                    var gameOver = document.createElement("h1")
+                    gameOver.innerHTML = "Game Over"
+                    var head = document.getElementById("title")
+                    head.appendChild(gameOver) 
+                }
+            }
+        }
+    }
+}
+
+
+var ghostWord = function() {
+        var ledger = document.createElement("ul")
+        ledger.setAttribute("style", "list-style: none;")
+        var sec = document.getElementById("word")
+        sec.appendChild(ledger)
+        console.log(word.length)
+
+        for(i=0; i<word.length; i++){
+            var list = document.createElement("li")
+            list.innerHTML = " _ "
+            list.setAttribute("style", "display: inline; font-size: 6em")
+            list.id = word[i] + "_"
+            ledger.appendChild(list)    
+        }
+    }
+
+choiceBar()
+
+var canvas = document.getElementById("screen")
+var context = canvas.getContext("2d")
+
+
+
 
 
 var line = function(x1,y1,x2,y2){
@@ -65,13 +132,10 @@ var struct = function(){
     noose()
 }
 
-var ledger = document.getElementById("#word")
-var 
-
-    for(i=0; i<word.length; i++){
-        ledger.innerHTML("_")
-    }
+var sketchMan = [leftLeg, rightLeg, leftArm, rightArm, body, head]
 
 struct()
+ghostWord()
+
 
 }
