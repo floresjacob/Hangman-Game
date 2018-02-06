09442474d28d840f1968639a947d1a01a81fc986
Jacob Flores <jacob.e.flores562@gmail.com>
2018-02-05 19:26:42 -0800

    multiple words

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,20 +3,30 @@ window.onload = function () {
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h",
 "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
 "t", "u", "v", "w", "x", "y", "z"]
-var word = ["p","a","d","d","i","e","s"]
 
-var bar = document.getElementById("choice")
+var words = ["paddies", "soybeans", "rice"]
+var over = false
+var win = false
+var wordChoice = function() {
+    var num = Math.floor(Math.random()*words.length)
+    var word = words[num].split("")
+    points = 0
+    return word
+}
+
+bar = document.getElementById("choice")
 bar.setAttribute("style", "margin: auto;")
 var letArray = []
 var attempts = 5
-var choiceBar = function() {
+
+ choiceBar = function() {
     
     for(i=0; i<alphabet.length; i++){
         letter = document.createElement("button")
         letter.setAttribute("type","button")
         letter.classList.add("btn")
         letter.classList.add("btn-outline-success")
-        letter.setAttribute("id", "press")
+        letter.classList.add("press")
         letter.innerHTML = alphabet[i]
         select();
         bar.appendChild(letter)  
@@ -29,29 +39,36 @@ var select = function() {
         letArray.push(this.innerHTML)
         console.log(letArray)
         var bool = false
+        console.log(word.length)
         for(i=0; i<word.length; i++){
             if(this.innerHTML === word[i]){
-                var hit = document.getElementById(word[i]+"_")
+                var hit = document.getElementById(i)
                 hit.innerHTML = this.innerHTML
                 bool = true
                 this.onclick = null
+                points++
+                console.log(points)
+                if(points == word.length){
+                    var gameOver = document.createElement("h1")
+                    gameOver.innerHTML = "You Win!"
+                    var head = document.getElementById("title")
+                    head.appendChild(gameOver) 
+                    btnMake()
+                }
             }
             else if(i===word.length-1 && bool===false){
                 sketchMan[attempts]()
                 attempts--
-                if(attempts < 0){
-                    var gameOver = document.createElement("h1")
-                    gameOver.innerHTML = "Game Over"
-                    var head = document.getElementById("title")
-                    head.appendChild(gameOver) 
+                if(attempts < 0){    
+                    gameOver()
                 }
             }
         }
     }
 }
 
-
 var ghostWord = function() {
+        word = wordChoice()
         var ledger = document.createElement("ul")
         ledger.setAttribute("style", "list-style: none;")
         var sec = document.getElementById("word")
@@ -62,7 +79,7 @@ var ghostWord = function() {
             var list = document.createElement("li")
             list.innerHTML = " _ "
             list.setAttribute("style", "display: inline; font-size: 6em")
-            list.id = word[i] + "_"
+            list.id = i
             ledger.appendChild(list)    
         }
     }
@@ -71,10 +88,6 @@ choiceBar()
 
 var canvas = document.getElementById("screen")
 var context = canvas.getContext("2d")
-
-
-
-
 
 var line = function(x1,y1,x2,y2){
     context.beginPath()
@@ -130,6 +143,33 @@ var struct = function(){
     pole()
     bar()
     noose()
+}
+
+var gameOver = function() {
+        var gameOver = document.createElement("h1")
+        gameOver.innerHTML = "Game Over"
+        var head = document.getElementById("title")
+        head.appendChild(gameOver) 
+        over = true
+}
+
+var playAgain = function() {
+    ghostWord()
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    struct()
+    choiceBar()
+}
+
+var btnMake = function() {
+    restart = document.createElement("button")
+    restart.innerHTML = "Play Again?"
+    var head = document.getElementById("title")
+    head.appendChild(restart) 
+
+    restart.onclick = function() {
+        playAgain()
+    }
+
 }
 
 var sketchMan = [leftLeg, rightLeg, leftArm, rightArm, body, head]
